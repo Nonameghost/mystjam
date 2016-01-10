9da6d4269aa17a894a5a605858e76f39b95f5278
Neil Sveri <nxsveri@gmail.com>
2016-01-10 11:04:25 -0500

    Initial Commit

---
+++ Assets/Target.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour {
+
+	public enum TargetType{
+		Move,
+		Inspect,
+		Trigger
+	}
+
+	public TargetType type;
+}
---
+++ Assets/TargetActionController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TargetActionController : MonoBehaviour {
+
+	public LayerMask targetMask;
+	public Color defaultCursorColor;
+	public Color highlightCursorColor;
+
+	[SerializeField]
+	private Image cursorImageObject;
+	[SerializeField]
+	private Sprite defaultCursor;
+	[SerializeField]
+	private Sprite moveCursor;
+	[SerializeField]
+	private Sprite triggerCursor;
+	[SerializeField]
+	private Sprite inspectCursor;
+
+	private Collider lastColliderHit;
+	private Target currentTarget;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.frameCount%3==0)
+		{
+			RaycastHit hit;
+			if(Physics.Raycast(transform.position,transform.forward, out hit, 100.0f,targetMask,QueryTriggerInteraction.Collide))
+			{
+				if(hit.collider != lastColliderHit)
+				{
+					currentTarget = hit.collider.GetComponent<Target>();
+					UpdateCursorUI(currentTarget.type);
+					lastColliderHit = hit.collider;
+				}
+			}
+			else{
+				UpdateCursorUI();
+				currentTarget = null;
+				lastColliderHit = null;
+			}
+		}
+	}
+
+	private void UpdateCursorUI(Target.TargetType type)
+	{
+		cursorImageObject.color = highlightCursorColor;
+		if(type.Equals(Target.TargetType.Move))
+		{
+			cursorImageObject.sprite = moveCursor;
+		}
+		else if(type.Equals(Target.TargetType.Inspect))
+		{
+			cursorImageObject.sprite = inspectCursor;
+		}
+		else if(type.Equals(Target.TargetType.Trigger))
+		{
+			cursorImageObject.sprite = triggerCursor;
+		}
+	}
+
+	private void UpdateCursorUI()
+	{
+		cursorImageObject.color = defaultCursorColor;
+		cursorImageObject.sprite = defaultCursor;
+	}
+}
